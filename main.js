6a2ed2c6436e1955c6620a5c27d84b915625535e
Ibrahim Abdelazim <93209660+Ibrahim7201@users.noreply.github.com>
2022-06-18 01:04:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-const main = document.getElementById("main");
+const content = document.getElementById("content");
+const Surah = document.getElementById("surah");
+const Num = document.getElementById("num");
 let ayahNum = getRandomInt(1, 6236);
 fetch(`https://api.alquran.cloud/v1/ayah/${ayahNum}/ar.asad`)
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    main.innerHTML = data.data.text;
+    content.innerHTML = data?.data?.text;
+    Surah.innerHTML = `${data?.data?.surah?.name}`;
+    Num.innerHTML = `آية ${data?.data?.numberInSurah}`;
   })
   .catch((error) => {
     console.log(error);
